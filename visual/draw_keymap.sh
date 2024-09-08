37d7972982e0f52b9e96de577de4caf6d0ff0312
Falk Zwimpfer <24669719+FalkZ@users.noreply.github.com>
2024-09-08 19:01:50 +0200

    removed old files

--- visual/draw_keymap.sh
+++
@@ -1,4 +0,0 @@
-pipx run keymap-drawer parse -c 10 -z ./config/corne.keymap >./visual/corne_keymap.yaml
-deno run --allow-read --allow-write ./visual/transformKeymap.ts
-pipx run keymap-drawer draw ./visual/transformed_keymap.yaml >./visual/keymap.svg
-deno run -A --unstable ./visual/preview.ts
